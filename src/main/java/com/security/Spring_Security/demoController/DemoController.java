2662d9b43174fc27964feb50ada7c67815cc6d7c
KhomotsoJerry <jerry@khomotso.com>
2024-07-16 21:02:48 +0200

    added demo package for running my test cases

---
+++ src/main/java/com/security/Spring_Security/demoController/DemoController.java
@@ -0,0 +1,15 @@
+package com.security.Spring_Security.demoController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/demo")
+@RestController
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> getStringFromSecuredAPI(){
+        return ResponseEntity.ok("getting this from secured api");
+    }
+}
